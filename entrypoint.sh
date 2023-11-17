2c58953e3cc7e1d229c1646876e5fcad941899a9
Pankaj <spo0nman@users.noreply.github.com>
2023-11-17 16:12:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,19 +44,18 @@ mkdir tmp/ && cd tmp/
 curl -sSL https://github.com/gohugoio/hugo/releases/download/${HUGO_VERSION}/hugo_extended_${HUGO_VERSION:1}_Linux-64bit.tar.gz | tar -xvzf-
 mv hugo /usr/local/bin/
 cd .. && rm -rf tmp/
-cd ${GITHUB_WORKSPACE}
 hugo version || exit 1
 
 # Build
 if [ "$MINIFY" = "true" ]; then
+  cd /home/runner/work/fableable/fableable/fableable.com
   hugo --minify
 else
+  cd /home/runner/work/fableable/fableable/fableable.com
   hugo
 fi
 
 # Deploy as configured in your repo
-cd ${WORKDIR}
-pwd
 cd /home/runner/work/fableable/fableable/fableable.com
 hugo deploy
 
